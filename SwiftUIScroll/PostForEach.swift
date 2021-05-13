a56df704a06a08e4de86d1f305a209ec3bba28f2
Gualtiero <gualtiero.frigerio@gmail.com>
2021-05-13 15:30:05 +0200

    added RefreshableScrollView

--- SwiftUIScroll/PostForEach.swift
+++ SwiftUIScroll/PostForEach.swift
@@ -12,23 +12,36 @@ struct PostForEach: View {
     var lazy:Bool = false
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                if lazy {
-                    LazyVStack {
-                        ForEach(posts) { post in
-                            PostView(post: post)
-                        }
-                    }
+        RefreshableScrollView(action: refreshList) {
+            if isLoading {
+                VStack {
+                    ProgressView()
+                    Text("loading...")
                 }
-                else {
-                    VStack {
-                        ForEach(posts) { post in
-                            PostView(post: post)
-                        }
+            }
+            if lazy {
+                LazyVStack {
+                    ForEach(posts) { post in
+                        PostView(post: post)
                     }
                 }
             }
+            else {
+                VStack {
+                    ForEach(posts) { post in
+                        PostView(post: post)
+                    }
+                }
+            }
+        }
+    }
+    
+    @State private var isLoading = false
+    
+    private func refreshList() {
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            isLoading = false
         }
     }
 }
